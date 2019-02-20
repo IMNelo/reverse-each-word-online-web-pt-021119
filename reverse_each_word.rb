a70253f6ad39c9b3ee10f73a317265f834390964
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-02-20 06:28:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,7 +5,7 @@ end
 
 
 #Reverse each word with ".each"
-# returns that same sentence with each word reversed in place.
+#Returns that same sentence with each word reversed in place.
   #First solve it using .each Then write the same method using .collect to see the difference.
   #turn the string into an array
   #.reverse reverses an array
