91865e06f6a2f6b0877b47289728f7dfb31f453f
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-02-20 06:28:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -4,6 +4,9 @@ def reverse_each_word("Hello there, and how are you?")
 end
 
 
+
+
+
 #Reverse each word with ".each"
 #Returns that same sentence with each word reversed in place.
   #First solve it using .each Then write the same method using .collect to see the difference.
