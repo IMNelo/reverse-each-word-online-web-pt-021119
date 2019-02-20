c7a75cdab0fc2ed0411a73337f4d5f586116b717
Agnelo Nelo Ribeiro <agnelo.ribeiro@gmail.com>
2019-02-20 07:46:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -8,26 +8,5 @@ def reverse_each_word(string)
 end
 
 
-#Create `reverse_each_word` method that takes in a string argument
-#Returns that same sentence with each word reversed in place.
-  #First solve it using `.each`
-  #Then utilize the same method using `.collect` to see the difference
-#!TIP! 
-  #turn the string into an array
-  #`.reverse` reverses an array
-
-def reverse_each_word(string)
 
 
-  original_array = string.split(" ")
-  return_array = []
-  original_array.each do|string|
-    return_array << string.reverse
-  end
-  return_array.join(" ")
-end
-
-#Can't use enumerators on a string so, turn string into an array, use `.split`
-#Create empty array, you'll use it to return the changed sentence later
-#Iterate through the orig_sentence w/ `.each`
-#Add the string to the empty area created earlierOperate on the chgd_sentence arraythrough the orig_sentence
